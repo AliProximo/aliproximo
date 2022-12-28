345e7c9b6e1e3f3b239b2809445f3c46badfff78
Thales Menezes <thlmenezes@pm.me>
2022-12-27 22:20:11 -0300

    💄 customize theme to match project design

--- apps/nextjs/tailwind.config.cjs
+++ apps/nextjs/tailwind.config.cjs
@@ -1,4 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   presets: [require("@acme/tailwind-config")],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [{
+      light: {
+        ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+        neutral: "#2A586C",
+        '.input-bordered': {
+          'border-color': '#000000',
+        },
+        '.btn-outline': {
+          'border-color': '#000000',
+        }
+      },
+    }],
+  }
 };
