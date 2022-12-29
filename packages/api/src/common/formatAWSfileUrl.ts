18d537783fb1327429ddc48cb9e1defce42daff6
Thales Menezes <thlmenezes@pm.me>
2022-12-29 14:21:24 -0300

    🐛 need to url encode filename for AWS links

--- packages/api/src/common/formatAWSfileUrl.ts
+++ packages/api/src/common/formatAWSfileUrl.ts
@@ -1,5 +1,5 @@
 import { env } from '../env/server.mjs'
 
 export function formatAWSfileUrl(filename: string) {
-  return `https://${env.S3_BUCKET_NAME}.s3.${env.S3_REGION}.amazonaws.com/${filename}`
+  return `https://${env.S3_BUCKET_NAME}.s3.${env.S3_REGION}.amazonaws.com/${encodeURIComponent(filename)}`
 }
