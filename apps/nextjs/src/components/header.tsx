8d9727e3183c58384ebb3923b5168c98d8a49a05
Thales Menezes <thlmenezes@pm.me>
2022-12-29 15:06:16 -0300

    ✨ add link to root in header logo

--- apps/nextjs/src/components/header.tsx
+++ apps/nextjs/src/components/header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = React.HTMLAttributes<HTMLHeadingElement>;
 
@@ -8,13 +9,15 @@ export const Header: React.FC<Props> = ({ className, ...props }) => {
       className={`bg-neutral min-h-8 sticky top-0 left-0 z-10 flex flex-col justify-center ${className}`}
       {...props}
     >
-      <Image
-        className="ml-12"
-        src={"/brand.png"}
-        alt="AliPróximo"
-        width={131}
-        height={20}
-      />
+      <Link href={"/admin"}>
+        <Image
+          className="ml-12"
+          src={"/brand.png"}
+          alt="AliPróximo"
+          width={131}
+          height={20}
+        />
+      </Link>
     </header>
   );
 };
